aa1b6e699a36ec9bcb103e084c85494644dacf89
Alexandra Dinca <alexandradi2003@gmail.com>
2025-05-10 15:44:45 +0300

    Adding skeleton to git

---
+++ src/solver_blas.c
@@ -0,0 +1,12 @@
+/*
+ * Tema 2 ASC
+ * 2024 Spring
+ */
+#include "utils.h"
+
+/* 
+ * Add your BLAS implementation here
+ */
+double* my_solver(int N, double *A, double *B, double *x) {
+	return NULL;
+}
---
+++ src/solver_neopt.c
@@ -0,0 +1,12 @@
+/*
+ * Tema 2 ASC
+ * 2025 Spring
+ */
+#include "utils.h"
+
+/*
+ * Add your unoptimized implementation here
+ */
+double* my_solver(int N, double *A, double *B, double *x) {
+	return NULL;
+}
