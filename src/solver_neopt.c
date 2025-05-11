5f42a3f9ad3fd732df7cc0003b94f845cebbda3f
Alexandra Dinca <alexandradi2003@gmail.com>
2025-05-11 15:56:57 +0300

    Implemented non optimal version and began optimal version.

--- src/solver_neopt.c
+++ src/solver_neopt.c
@@ -8,5 +8,76 @@
  * Add your unoptimized implementation here
  */
 double* my_solver(int N, double *A, double *B, double *x) {
-	return NULL;
+    //  Transpose A
+    double *At = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            At[i * N + j] = A[j * N + i];
+        }
+    }
+
+    //  C = B * At
+    double *C = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i * N + j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i * N + j] += B[i * N + k] * At[k * N + j];
+            }
+        }
+    }
+
+    //  Transpose C
+    double *Ct = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            Ct[i * N + j] = C[j * N + i];
+        }
+    }
+
+    //  D = Ct * A
+    double *D = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            D[i * N + j] = 0;
+            for (int k = 0; k < N; k++) {
+                D[i * N + j] += Ct[i * N + k] * A[k * N + j];
+            }
+        }
+    }
+
+    double *y = malloc(N * sizeof(double));
+    for (int iter = 0; iter < N; iter++) {
+        //  y = Ct * x
+        for (int i = 0; i < N; i++) {
+            y[i] = 0;
+            for (int j = 0; j < N; j++) {
+                y[i] += Ct[i * N + j] * x[j];
+            }
+        }
+
+        //  x = C * y
+        for (int i = 0; i < N; i++) {
+            x[i] = 0;
+            for (int j = 0; j < N; j++) {
+                x[i] += C[i * N + j] * y[j];
+            }
+        }
+    }
+
+    //  y = D * x
+    for (int i = 0; i < N; i++) {
+        y[i] = 0;
+        for (int j = 0; j < N; j++) {
+            y[i] += D[i * N + j] * x[j];
+        }
+    }
+
+    //  Free memory
+    free(C);
+    free(D);
+    free(At);
+    free(Ct);
+
+    return y;
 }
--- src/solver_opt.c
+++ src/solver_opt.c
@@ -8,5 +8,83 @@
  * Add your optimized implementation here
  */
 double* my_solver(int N, double *A, double *B, double *x) {
-	return NULL;	
+    register double suma = 0.0;
+
+    //  Transpose A
+    double *At = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            At[i * N + j] = A[j * N + i];
+        }
+    }
+
+    //  C = B * At
+    double *C = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            suma = 0.0;
+            for (int k = 0; k < N; k++) {
+                suma += B[i * N + k] * At[k * N + j];
+            }
+            C[i * N + j] = suma;
+        }
+    }
+
+    //  Transpose C
+    double *Ct = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            Ct[i * N + j] = C[j * N + i];
+        }
+    }
+
+    //  D = Ct * A
+    double *D = malloc(N * N * sizeof(double));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            suma = 0.0;
+            for (int k = 0; k < N; k++) {
+                suma += Ct[i * N + k] * A[k * N + j];
+            }
+            D[i * N + j] = suma;
+        }
+    }
+
+    double *y = malloc(N * sizeof(double));
+    for (int iter = 0; iter < N; iter++) {
+        //  y = Ct * x
+        for (int i = 0; i < N; i++) {
+            suma = 0.0;
+            for (int j = 0; j < N; j++) {
+                suma += Ct[i * N + j] * x[j];
+            }
+            y[i] = suma;
+        }
+
+        //  x = C * y
+        for (int i = 0; i < N; i++) {
+            suma = 0.0;
+            for (int j = 0; j < N; j++) {
+                suma += C[i * N + j] * y[j];
+            }
+            x[i] = suma;
+        }
+    }
+
+    //  y = D * x
+    for (int i = 0; i < N; i++) {
+        suma = 0.0;
+        for (int j = 0; j < N; j++) {
+            suma += D[i * N + j] * x[j];
+        }
+        y[i] = suma;
+    }
+
+    //  Free memory
+    free(C);
+    free(D);
+    free(At);
+    free(Ct);
+
+    return y;
 }
